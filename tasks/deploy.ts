e40b50bceab755d6270647f1310d102f736887dc
23Fold <elod@sophon.xyz>
2025-03-27 18:03:26 +0100

    chore: further cleaning

--- tasks/deploy.ts
+++ tasks/deploy.ts
@@ -1,20 +1,42 @@
-import { task, types } from "hardhat/config";
-import { Reclaim } from "../src/types";
-import { utils, Wallet as ZkWallet, Provider } from "zksync-ethers";
+import { task } from "hardhat/config";
+import { utils, Wallet as ZkWallet, Provider, Wallet } from "zksync-ethers";
 import { Deployer } from "@matterlabs/hardhat-zksync";
-import { Wallet } from "ethers";
 import * as dotenv from "dotenv";
 
 dotenv.config();
+
+export const getProvider = (hre: any) => {
+  const rpcUrl = hre.network.config.url;
+  if (!rpcUrl)
+    throw `⛔️ RPC URL wasn't found in "${hre.network.name}"! Please add a "url" field to the network config in hardhat.config.ts`;
+
+  // Initialize ZKsync Provider
+  const provider = new Provider(rpcUrl);
+
+  return provider;
+};
+
+export const getWallet = (hre: any, privateKey?: string) => {
+  if (!privateKey) {
+    // Get wallet private key from .env file
+    if (!process.env.WALLET_PRIVATE_KEY) throw "⛔️ Wallet private key wasn't found in .env file!";
+  }
+
+  const provider = getProvider(hre);
+
+  // Initialize ZKsync Wallet
+  const wallet = new Wallet(privateKey ?? process.env.WALLET_PRIVATE_KEY!, provider);
+
+  return wallet;
+};
+
 task("deploy", "Deployes the Reclaim contract").setAction(async (taskArgs, hre) => {
-  const sk = process.env.WALLET_PRIVATE_KEY || "";
-  console.log(sk);
-
-  const zkProvider = Provider.getDefaultProvider(1); // or another zkSync provider
-
-  const zkWallet = new ZkWallet(sk, zkProvider, hre.ethers.provider);
-
+  const zkWallet = getWallet(hre);
   const deployer = new Deployer(hre, zkWallet);
+
+  const deployerWallet = deployer.zkWallet;
+
+  console.log(deployerWallet.address);
 
   const artifact = await deployer.loadArtifact("Reclaim");
 
@@ -26,11 +48,12 @@ task("deploy", "Deployes the Reclaim contract").setAction(async (taskArgs, hre) 
     }
   );
 
-  const contract = await deployer.deploy(artifact, [], "create2", {
+  const contract = await deployer.deploy(artifact, [], "create", {
     customData: {
       paymasterParams: params,
       gasPerPubdata: utils.DEFAULT_GAS_PER_PUBDATA_LIMIT,
     },
   });
-  console.log(contract.address);
+  const CA = await contract.getAddress();
+  console.log(CA);
 });
